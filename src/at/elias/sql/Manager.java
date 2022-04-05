cd3613f5947cd66afd38131acef67e31d5b6c120
Elias <meiere855@gmail.com>
2022-04-05 22:40:59 +0200

    Implemented Bugfix for Database Timeout

--- src/at/elias/sql/Manager.java
+++ src/at/elias/sql/Manager.java
@@ -3,18 +3,42 @@ package at.elias.sql;
 public class Manager {
 
     public void createTable(String table) {
-        new SqlManager().createTable(table);
+        try {
+            new SqlManager().createTable(table);
+        } catch (Exception e) {
+            SQLAPI.getInstance().getMySQL().disconnectFromMySQL();
+            SQLAPI.getInstance().getMySQL().connectToMySQL();
+            createTable(table);
+        }
     }
 
     public void create(String id, String table, String type) {
-        new SqlManager().create(id, table, type);
+        try {
+            new SqlManager().create(id, table, type);
+        } catch (Exception e) {
+            SQLAPI.getInstance().getMySQL().disconnectFromMySQL();
+            SQLAPI.getInstance().getMySQL().connectToMySQL();
+            create(id, table, type);
+        }
     }
 
     public void setString(String id, String table, String type, String value) {
-        new SqlManager().setString(id, table, type, value);
+        try {
+            new SqlManager().setString(id, table, type, value);
+        } catch (Exception e) {
+            SQLAPI.getInstance().getMySQL().disconnectFromMySQL();
+            SQLAPI.getInstance().getMySQL().connectToMySQL();
+            setString(id, table, type, value);
+        }
     }
 
     public String getString(String id, String table, String type) {
-        return new SqlManager().getString(id, table, type);
+        try {
+            return new SqlManager().getString(id, table, type);
+        } catch (Exception e) {
+            SQLAPI.getInstance().getMySQL().disconnectFromMySQL();
+            SQLAPI.getInstance().getMySQL().connectToMySQL();
+            return getString(id, table, type);
+        }
     }
 }
